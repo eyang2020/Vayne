8d05b9e67077c0f978c6ff5a5b70dd71ea12878c
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-06-11 19:57:08 -0500

    added dm functionality + cache support

--- cogs/sale.py
+++ cogs/sale.py
@@ -1,7 +1,7 @@
 import os
 import pymongo
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from datetime import datetime
 from requests_html import AsyncHTMLSession
 
@@ -11,45 +11,113 @@ cluster = pymongo.MongoClient(MONGODB_AUTH)
 db = cluster.test
 collectionUserToSkin = db['UserToSkin']
 collectionSkinToUser = db['SkinToUser']
+collectionSaleInfo = db['SaleInfo']
 
 class Sale(commands.Cog):
+
+    # saleCache: cache for current sale. format: {itemName: (fullPrice, discountPrice)}
+    # durationCache: current sale duration
+    saleCache = {}
+    durationCache = ''
+    
     def __init__(self, client):
         self.client = client
         self.color = discord.Colour(0x5500ff)
         self.session = AsyncHTMLSession()
+        self.checkSale.start()
 
     # Events
     @commands.Cog.listener()
     async def on_ready(self):
         print('Sale cog is online.')
 
+    def cog_unload(self):
+        self.checkSale.cancel()
+
     @commands.Cog.listener()
     async def on_cog_unload_event(self):
         await self.session.close()
+
+    # Tasks
+    @tasks.loop(hours=24)
+    async def checkSale(self):
+        print('Checking for new sale...')
+        #print(self.saleCache)
+        res = await self.session.get('https://leagueoflegends.fandom.com/wiki/Sales')
+        curDuration = res.html.xpath('//*[@id="onsale"]/div/div[1]/text()')[0]
+        docSaleInfo = collectionSaleInfo.find_one({'saleInfo': 'saleInfo'})
+        storedDuration = docSaleInfo['duration']
+        # check on whether a new sale as begun or if cache needs to be initialized
+        newSale = (curDuration != storedDuration)
+        if newSale or not self.saleCache:
+            # update cache
+            self.saleCache = {}
+            res = await self.session.get('https://leagueoflegends.fandom.com/wiki/Sales')
+            table = res.html.xpath('//*[@id="onsale"]/div/div[2]')[0]
+            banners = table.find('.centered-grid-icon')
+            for banner in banners:
+                info = banner.text.split('\n')
+                self.saleCache[info[0]] = (info[1], info[2])
+            self.durationCache = curDuration
+            # if there is a new sale: update db and send a round of dm's to notify wishlist
+            if newSale: 
+                # update db
+                collectionSaleInfo.update_one({'saleInfo': 'saleInfo'}, {'$set': {'duration': curDuration}}, upsert=True)
+                # for each skin, dm the users who have it wishlisted
+                for itemName, priceTuple in self.saleCache.items():
+                    docSkinToUser = collectionSkinToUser.find_one({'skin': itemName})
+                    if not docSkinToUser:
+                        # if no doc was found, this was a champion on sale
+                        continue
+                    userIds = docSkinToUser['userId']
+                    if len(userIds) == 0:
+                        continue
+                    # create the embed
+                    embed = discord.Embed(
+                        color=self.color
+                    )
+                    formattedStr = f'```ml\n{itemName} {priceTuple[0]} → {priceTuple[1]}```\n'
+                    embed.add_field(name='A skin you wishlisted is on sale!', value=formattedStr, inline=False)
+                    for userId in userIds:
+                        user = self.client.get_user(userId)
+                        await user.send(embed=embed)
+
+        print('Finished checking for sale.')
+        #print(self.saleCache)
+
+    @checkSale.before_loop
+    async def beforeCheckSale(self):
+        await self.client.wait_until_ready()
 
     # Commands
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def sale(self, ctx):
-        res = await self.session.get('https://leagueoflegends.fandom.com/wiki/Sales')
-        durationStr = '```' + res.html.xpath('//*[@id="onsale"]/div/div[1]/text()')[0]
-        table = res.html.xpath('//*[@id="onsale"]/div/div[2]')[0]
-        banners = table.find('.centered-grid-icon')
-
+        if not self.saleCache:
+            # fill cache
+            res = await self.session.get('https://leagueoflegends.fandom.com/wiki/Sales')
+            self.durationCache = res.html.xpath('//*[@id="onsale"]/div/div[1]/text()')[0]
+            table = res.html.xpath('//*[@id="onsale"]/div/div[2]')[0]
+            banners = table.find('.centered-grid-icon')
+            for banner in banners:
+                info = banner.text.split('\n')
+                self.saleCache[info[0]] = (info[1], info[2])
+        # create embed
+        durationStr = '```'
         championSaleStr = '```ml\n'
         skinSaleStr = '```ml\n'
         counter = 0
         # sale format: 5 champions first, then 15 skins
-        for banner in banners:
-            info = banner.text.split('\n')
-            formattedStr = f'{info[0]:<30} {info[1]:>4} → {info[2]:>4}\n'
+        for itemName, priceTuple in self.saleCache.items():
+            formattedStr = f'{itemName:<30} {priceTuple[0]:>4} → {priceTuple[1]:>4}\n'
             if counter < 5: championSaleStr += formattedStr
             else: skinSaleStr += formattedStr
             counter += 1
+        durationStr += self.durationCache
 
+        durationStr += '```'
         championSaleStr += '```'
         skinSaleStr += '```'
-        durationStr += '```'
 
         embed = discord.Embed(
             title='Current Sale',
@@ -70,6 +138,7 @@ class Sale(commands.Cog):
             color=self.color
         )
 
+        # todo: add support for case when doc is None
         doc = collectionUserToSkin.find_one({'userId': userId})
         skins = doc['skins']
 
@@ -148,4 +217,4 @@ class Sale(commands.Cog):
 def setup(client):
     client.add_cog(Sale(client))
 
-# todo: add skin wishlist / notification system (through dm)
+# todo: add emotes for all champions (big yikes)
